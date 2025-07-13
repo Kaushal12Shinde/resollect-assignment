a5a12ba60e0b0ffd172121a96e72dbc35964baf5
Kaushal12Shinde <kaushushinde12@gmail.com>
2025-07-14 01:56:09 +0530

    feat/ add firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import React from 'react';
+import TaskPage from './pages/TaskPage';
 
 function App() {
   return (
-   <>
-   </>
-  )
+    <div className="max-w-4xl mx-auto p-4">
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
